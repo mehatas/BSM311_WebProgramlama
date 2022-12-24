a1429296fa59e79f53ebab67d80d53c9362e4b89
hakanyilmaz96 <hakan.yilmaz15@ogr.sakarya.edu.tr>
2022-12-24 15:40:05 +0300

    Controllerlar eklendi.

---
+++ WebProje/WebProje/Controllers/FilmlerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProje.Data;
+
+namespace WebProje.Controllers
+{
+    public class FilmlerController : Controller
+    {
+        private readonly Veritabani _context;
+
+        public FilmlerController(Veritabani context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var tumFilmler = await _context.Filmler.ToListAsync();
+            return View();
+        }
+    }
+}

a70241eabd98e4c4679b56ef8fc084e01c974c9d
mehatas <83393818+mehatas@users.noreply.github.com>
2022-12-27 01:41:58 +0300

    Proje dosyasi guncellendi.

---
+++ WebProje/eBiletUygulaması/eBiletUygulaması/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eBiletUygulaması.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebProje/eBiletUygulaması/eBiletUygulaması/Program.cs
@@ -0,0 +1,40 @@
+using eBiletUygulamasý.Data.Cart;
+using eBiletUygulamasý.Data.Services;
+using eBiletUygulamasý.Data;
+using eBiletUygulamasý.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Movies}/{action=Index}/{id?}");
+});
+
+app.Run();
